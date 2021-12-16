c849b36bce15250d36803aa849bbc277228649b7
sauna1137 <subscription.search@gmail.com>
2021-12-16 17:44:01 +0900

    CalcModel 追加　first commit

---
+++ PracticeMVP3/Model/CalcModel.swift
@@ -0,0 +1,18 @@
+//
+//  CalucModel.swift
+//  PracticeMVP3
+//
+//  Created by KS on 2021/12/16.
+//
+
+import Foundation
+
+class CalcModel {
+
+    var resultNum = 0
+
+    func calculateNums(num1: Int, num2: Int) -> Int {
+        return num1 + num2
+    }
+}
+
