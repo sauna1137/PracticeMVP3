0c70d6a805d2fc51868c937bf89c1453658c9072
sauna1137 <subscription.search@gmail.com>
2021-12-16 18:12:34 +0900

    enum追加 AlertModel削除により処理を追加

--- PracticeMVP3/Presenter/Presenter.swift
+++ PracticeMVP3/Presenter/Presenter.swift
@@ -17,16 +17,34 @@ class Presenter {
 
     var delegate: PresenterDelegate?
     var calcModel = CalcModel()
-    var alertModle = AlertModel()
 
     func calcButtonDidTap(num1: Int, num2: Int){
         let result = calcModel.calculateNums(num1: num1, num2: num2)
         delegate?.calcurated(result: result)
     }
 
-    func textFieldInputError() {
-        let alert = alertModle.inputedTextFieldError()
+    enum alertType {
+        case noValue
+        case noInt
+    }
+
+    func textFieldInputError(alertType: alertType) {
+
+        let alert: UIAlertController
+
+        switch alertType {
+        case .noValue: alert = inputedTextFieldError(message: "値が入力されていません")
+        case .noInt: alert = inputedTextFieldError(message: "数値以外の値が入力されています。")
+        }
+
         delegate?.passAlertController(alert: alert)
+    }
+
+    func inputedTextFieldError(message: String) -> UIAlertController {
+        let alert = UIAlertController(title: message, message: "数字を入力してください", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        return alert
     }
 
 }
